f619c08d190acb9ff6f91946b32c7f2a756e6c20
trailmax <trailmax1@gmail.com>
2016-08-30 00:48:53 +0100

    set of TODO items for the next round

--- NSaga/SagaMediator.cs
+++ NSaga/SagaMediator.cs
@@ -34,31 +34,13 @@ namespace NSaga
             {
                 throw new ArgumentException("CorrelationId was not provided in the message. Please make sure you assign CorrelationId before initiating your Saga");
             }
+            //TODO Find saga
             //var saga = sagaRepository.Find(sagaMessage.CorrelationId);
 
-            //var initiatingMessage = sagaMessage as IInitiatingSagaMessage;
-            //if (initiatingMessage != null && saga != null)
-            //{
-            //    // saga with this CorrelationID already exists, can't initiate it again
-            //    throw new Exception("Saga have already been initialised");
-            //}
-
-            //if (initiatingMessage == null && saga == null)
-            //{
-            //    throw new Exception("Saga does not exist in the storage, but consumed message is not an initiating message. Please start Saga from IInitiatingMessage");
-            //}
-
-            //if (initiatingMessage != null)
-            //{
-            //    //var sagaType = sagaMessage.GetType(); //TODO find saga type that implements InitiatedBy<TSagaMessage>
-
-            //    //saga = sagaRepository.InitiateSaga(sagaType);
-            //    //var operationResult = saga.Initiate(sagaMessage);
-            //    //sagaRepository.Save(saga);
-            //    //return operationResult;
-            //}
-
-            //// saga should 
+            //TODO check that saga exists
+            //TODO check that saga implements ConsumerOf<TMsg>
+            //TODO execute saga message
+            //TODO save saga into repository
 
             throw new NotImplementedException();
         }
