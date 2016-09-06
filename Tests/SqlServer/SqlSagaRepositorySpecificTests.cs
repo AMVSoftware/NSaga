2355b209bb90bce91f53f6ecd0e238c06a66551d
trailmax <trailmax1@gmail.com>
2016-09-06 02:20:57 +0100

    Removing unimplemented templated tests

--- Tests/SqlServer/SqlSagaRepositorySpecificTests.cs
+++ Tests/SqlServer/SqlSagaRepositorySpecificTests.cs
@@ -1,9 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Configuration;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using FluentAssertions;
 using NSaga;
 using NSaga.SqlServer;
--- Tests/SqlServer/SqlSagaRepositoryTests.cs
+++ Tests/SqlServer/SqlSagaRepositoryTests.cs
@@ -1,13 +1,13 @@
-using NSaga;
-using NSaga.SqlServer;
+//using NSaga;
+//using NSaga.SqlServer;
 
-namespace Tests.SqlServer
-{
-    public class SqlSagaRepositoryTests : SagaRepositoryTestsTemplate
-    {
-        public SqlSagaRepositoryTests()
-        {
-            this.Sut = new SqlSagaRepository("TestingConnectionString", new DumbServiceLocator(), new JsonNetSerialiser());
-        }
-    }
-}
+//namespace Tests.SqlServer
+//{
+//    public class SqlSagaRepositoryTests : SagaRepositoryTestsTemplate
+//    {
+//        public SqlSagaRepositoryTests()
+//        {
+//            this.Sut = new SqlSagaRepository("TestingConnectionString", new DumbServiceLocator(), new JsonNetSerialiser());
+//        }
+//    }
+//}
