028df12103db6794c7788f2b69ac817db0533e05
trailmax <trailmax1@gmail.com>
2016-09-18 00:57:34 +0100

    Code documentation for SqlSagaRepository #6

--- src/NSaga.SqlServer/SqlSagaRepository.cs
+++ src/NSaga.SqlServer/SqlSagaRepository.cs
@@ -14,25 +14,57 @@ namespace NSaga.SqlServer
         private readonly Database database;
         private readonly IMessageSerialiser messageSerialiser;
 
-        public SqlSagaRepository(string connectionStringName, IServiceLocator serviceLocator,
-            IMessageSerialiser messageSerialiser)
+        /// <summary>
+        /// Initiates an instance of <see cref="SqlSagaRepository"/> with a connection string name.
+        /// Actual connection string is taken from your app.config or web.config
+        /// </summary>
+        /// <param name="connectionStringName">Name of a connection string to use from your config file</param>
+        /// <param name="serviceLocator">An instance implementing <see cref="IServiceLocator"/></param>
+        /// <param name="messageSerialiser">An instance implementing <see cref="IMessageSerialiser"/></param>
+        public SqlSagaRepository(string connectionStringName, IServiceLocator serviceLocator, IMessageSerialiser messageSerialiser)
         {
+            Guard.ArgumentIsNotNull(connectionStringName, nameof(connectionStringName));
+            Guard.ArgumentIsNotNull(serviceLocator, nameof(serviceLocator));
+            Guard.ArgumentIsNotNull(messageSerialiser, nameof(messageSerialiser));
+
             this.messageSerialiser = messageSerialiser;
             this.serviceLocator = serviceLocator;
             this.database = new Database(connectionStringName);
         }
 
-        public SqlSagaRepository(string connectionString, string providerName, IServiceLocator serviceLocator,
-            IMessageSerialiser messageSerialiser)
+        /// <summary>
+        /// Initiates an instance of <see cref="SqlSagaRepository"/> with a connection string and a connection provider name
+        /// </summary>
+        /// <param name="connectionString">String containing a connection string</param>
+        /// <param name="providerName">A provider name for the connection</param>
+        /// <param name="serviceLocator">An instance implementing <see cref="IServiceLocator"/></param>
+        /// <param name="messageSerialiser">An instance implementing <see cref="IMessageSerialiser"/></param>
+        public SqlSagaRepository(string connectionString, string providerName, IServiceLocator serviceLocator, IMessageSerialiser messageSerialiser)
         {
+            Guard.ArgumentIsNotNull(connectionString, nameof(connectionString));
+            Guard.ArgumentIsNotNull(providerName, nameof(providerName));
+            Guard.ArgumentIsNotNull(serviceLocator, nameof(serviceLocator));
+            Guard.ArgumentIsNotNull(messageSerialiser, nameof(messageSerialiser));
+
             this.messageSerialiser = messageSerialiser;
             this.serviceLocator = serviceLocator;
             this.database = new Database(connectionString, providerName);
         }
 
 
+        /// <summary>
+        /// Finds and returns saga instance with the given correlation ID.
+        /// You will get exceptions if TSaga does not match the actual saga data with the provided exception.
+        /// 
+        /// Actually creates an instance of saga from service locator, retrieves SagaData and Headers from the storage and populates the instance with these.
+        /// </summary>
+        /// <typeparam name="TSaga">Type of saga we are looking for</typeparam>
+        /// <param name="correlationId">CorrelationId to identify the saga</param>
+        /// <returns>An instance of the saga. Or Null if there is no saga with this ID.</returns>
         public TSaga Find<TSaga>(Guid correlationId) where TSaga : class
         {
+            Guard.ArgumentIsNotNull(correlationId, nameof(correlationId));
+
             var sql = Sql.Builder.Where("correlationId = @0", correlationId);
             var persistedData = database.SingleOrDefault<SagaData>(sql);
 
@@ -56,8 +88,18 @@ namespace NSaga.SqlServer
             return sagaInstance;
         }
 
+
+        /// <summary>
+        /// Persists the instance of saga into the database storage.
+        /// 
+        /// Actually stores SagaData and Headers. All other variables in saga are not persisted
+        /// </summary>
+        /// <typeparam name="TSaga"></typeparam>
+        /// <param name="saga"></param>
         public void Save<TSaga>(TSaga saga) where TSaga : class
         {
+            Guard.ArgumentIsNotNull(saga, nameof(saga));
+
             var sagaData = Reflection.Get(saga, "SagaData");
             var sagaHeaders = (Dictionary<String, String>) Reflection.Get(saga, "Headers");
             var correlationId = (Guid) Reflection.Get(saga, "CorrelationId");
@@ -69,7 +111,6 @@ namespace NSaga.SqlServer
                 CorrelationId = correlationId,
                 BlobData = serialisedData,
             };
-
 
             using (var transaction = database.GetTransaction())
             {
@@ -109,14 +150,27 @@ namespace NSaga.SqlServer
         }
 
 
+        /// <summary>
+        /// Deletes the saga instance from the storage
+        /// </summary>
+        /// <typeparam name="TSaga">Type of saga</typeparam>
+        /// <param name="saga">Saga to be deleted</param>
         public void Complete<TSaga>(TSaga saga) where TSaga : class
         {
+            Guard.ArgumentIsNotNull(saga, nameof(saga));
+
             var correlationId = (Guid)Reflection.Get(saga, "CorrelationId");
             Complete(correlationId);
         }
 
+        /// <summary>
+        /// Deletes the saga instance from the storage
+        /// </summary>
+        /// <param name="correlationId">Correlation Id for the saga</param>
         public void Complete(Guid correlationId)
         {
+            Guard.ArgumentIsNotNull(correlationId, nameof(correlationId));
+
             using (var transaction = database.GetTransaction())
             {
                 try
---
+++ src/NSaga/Guard.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Diagnostics;
+
+namespace NSaga
+{
+    internal static class Guard
+    {
+        [DebuggerHidden]
+        internal static void ArgumentIsNotNull(object value, string argument)
+        {
+            if (value == null)
+            {
+                throw new ArgumentNullException(argument);
+            }
+        }
+    }
+}
