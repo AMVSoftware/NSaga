ca6787b04d7f918e7580592a1e8ac9f89c5796eb
trailmax <trailmax1@gmail.com>
2016-10-17 02:36:05 +0100

    Trying to use builder with sql repository

---
+++ src/NSaga.SqlServer/BuilderExtension.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NSaga.SqlServer
+{
+    public static class BuilderExtension
+    {
+        public static SagaMediatorBuilder UseSqlServerStorage(this SagaMediatorBuilder builder, string connectionString)
+        {
+            //TODO register connectionstring for repository. DUH!
+
+            builder.UseRepository<SqlSagaRepository>();
+            return builder;
+        }
+    }
+}
