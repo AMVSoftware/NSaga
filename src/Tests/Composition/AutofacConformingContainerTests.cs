54bca211efd2ad28a149400180a931415fa1740b
trailmax <trailmax1@gmail.com>
2016-10-24 01:18:52 +0100

    Autofac conforming container

---
+++ src/Tests/Composition/AutofacConformingContainerTests.cs
@@ -0,0 +1,29 @@
+﻿using Autofac;
+using NSaga.Autofac;
+
+namespace Tests.Composition
+{
+    public class AutofacConformingContainerTests : ConformingContainerTestsBaseClass
+    {
+        public AutofacConformingContainerTests()
+        {
+            Sut = new AutofacConformingContainer(new ContainerBuilder());
+        }
+    }
+
+
+    public class AutofacNonBuilderConformingContainerTests : ConformingContainerTestsBaseClass
+    {
+        public AutofacNonBuilderConformingContainerTests()
+        {
+            var builder = new ContainerBuilder();
+            builder.RegisterType<SuperService>();
+            var container = builder.Build();
+
+            Sut = new AutofacConformingContainer(container);
+        }
+
+
+        public class SuperService { }
+    }
+}
