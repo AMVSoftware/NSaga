4ccb9b4d3b2bff08fed2abdf309e24c7dc8af1f1
trailmax <trailmax1@gmail.com>
2016-11-15 01:57:52 +0000

    package nugets

--- SolutionInfo.cs
+++ SolutionInfo.cs
@@ -2,4 +2,4 @@ using System.Reflection;
 
 [assembly: AssemblyVersion("0.1.0.0")]
 [assembly: AssemblyFileVersion("0.1.0.0")]
-[assembly: AssemblyInformationalVersion("0.1.0-buildProcess.1+141.Branch.buildProcess.Sha.9de6b9d71de452cd21e22f076d928e69fcc96d35")]
+[assembly: AssemblyInformationalVersion("0.1.0-buildProcess.1+142.Branch.buildProcess.Sha.50013ff1e3c3709ae530439f4b89aaa0fad9ad4d")]
