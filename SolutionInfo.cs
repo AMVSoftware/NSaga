4fd1975b8a9a86341cd578393996a4db61940fed
trailmax <trailmax1@gmail.com>
2016-11-17 22:24:39 +0000

    downgraded autofac to v 3.0.0

--- SolutionInfo.cs
+++ SolutionInfo.cs
@@ -2,4 +2,4 @@ using System.Reflection;
 
 [assembly: AssemblyVersion("1.0.1.0")]
 [assembly: AssemblyFileVersion("1.0.1.0")]
-[assembly: AssemblyInformationalVersion("1.0.1-NugetVersions.1+1.Branch.NugetVersions.Sha.6662bd8765106cddd218b05d110a204c4b2e1ed9")]
+[assembly: AssemblyInformationalVersion("1.0.1-NugetVersions.1+2.Branch.NugetVersions.Sha.5b41b68765fd2863b62b7be0ce000c32234eb861")]
