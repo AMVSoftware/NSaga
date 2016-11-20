c57d48ef6bfc08821d5cbb8db19efe404c3b3300
trailmax <trailmax1@gmail.com>
2016-11-20 00:23:50 +0000

    release notes for v1.1.0

--- ReleaseNotes.md
+++ ReleaseNotes.md
@@ -1,2 +1,5 @@
-### New in 0.1.0 (Released November 2016)
+### New in 1.1.0 (Released November 2016)
+* Static reference to SagaMediatorBuilder
+
+### New in 1.0.0 (Released November 2016)
 * First public release
--- SolutionInfo.cs
+++ SolutionInfo.cs
@@ -2,4 +2,4 @@ using System.Reflection;
 
 [assembly: AssemblyVersion("1.0.1.0")]
 [assembly: AssemblyFileVersion("1.0.1.0")]
-[assembly: AssemblyInformationalVersion("1.0.1-NugetVersions.1+2.Branch.NugetVersions.Sha.5b41b68765fd2863b62b7be0ce000c32234eb861")]
+[assembly: AssemblyInformationalVersion("1.0.1+10.Branch.master.Sha.a890afabcaa4edb52ed0ea27f6b1975ff3116df7")]
