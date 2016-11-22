2e5832e01273499099bba3890f3f30fb613ae950
trailmax <trailmax1@gmail.com>
2016-11-22 16:22:06 +0000

    update version number

--- SolutionInfo.cs
+++ SolutionInfo.cs
@@ -2,4 +2,4 @@ using System.Reflection;
 
 [assembly: AssemblyVersion("1.1.1.0")]
 [assembly: AssemblyFileVersion("1.1.1.0")]
-[assembly: AssemblyInformationalVersion("1.1.1-DotnetVersion.1+1.Branch.DotnetVersion.Sha.fba8c84a3df1c7f3007b43101b7d646630e866c2")]
+[assembly: AssemblyInformationalVersion("1.1.1+2.Branch.master.Sha.25d0abd7591766e152a1b7c418fb8e9a395eea03")]
