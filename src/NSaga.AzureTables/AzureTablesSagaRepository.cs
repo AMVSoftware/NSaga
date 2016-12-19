47fc946980aabe33ec633ec81c512b36c7e750f8
trailmax <trailmax1@gmail.com>
2016-12-19 20:50:44 +0000

    Created new project

---
+++ src/NSaga.AzureTables/AzureTablesSagaRepository.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NSaga.AzureTables
+{
+    public class AzureTablesSagaRepository : ISagaRepository
+    {
+        public TSaga Find<TSaga>(Guid correlationId) where TSaga : class, IAccessibleSaga
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Save<TSaga>(TSaga saga) where TSaga : class, IAccessibleSaga
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Complete<TSaga>(TSaga saga) where TSaga : class, IAccessibleSaga
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Complete(Guid correlationId)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
