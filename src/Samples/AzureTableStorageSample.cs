67c60986ec50d45ee56f344b9d66dd71bf10d7fc
trailmax <trailmax1@gmail.com>
2016-12-20 01:35:42 +0000

    todo items

---
+++ src/Samples/AzureTableStorageSample.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Samples
+{
+    public class AzureTableStorageSample
+    {
+        //TODO sample here
+        //TODO update wiki
+        //TODO sample in separate samples repo
+    }
+}
