a43a488c6ce7e2020cdd0174a2b4925cd54d6e30
trailmax <trailmax1@gmail.com>
2016-12-20 00:22:20 +0000

    Deleting saga table after tests

--- src/Tests/AzureTables/AzureTablesSagaRepositoryTests.cs
+++ src/Tests/AzureTables/AzureTablesSagaRepositoryTests.cs
@@ -4,13 +4,26 @@ using NSaga.AzureTables;
 
 namespace Tests.AzureTables
 {
-    public class AzureTablesSagaRepositoryTests : SagaRepositoryTestsTemplate
+    public class AzureTablesSagaRepositoryTests : SagaRepositoryTestsTemplate, IDisposable
     {
         public AzureTablesSagaRepositoryTests()
         {
             var connectionString = Environment.GetEnvironmentVariable("NSagaAzureTableStorage");// "UseDevelopmentStorage=true";
 
             this.Sut = new AzureTablesSagaRepository(new TableClientFactory(connectionString), new JsonNetSerialiser(), new DumbSagaFactory());
+        }
+
+
+
+        public void Dispose()
+        {
+            var connectionString = Environment.GetEnvironmentVariable("NSagaAzureTableStorage");
+
+            var tableClient = new TableClientFactory(connectionString).CreateTableClient();
+
+            var table = tableClient.GetTableReference("nsaga");
+
+            table.DeleteIfExists();
         }
     }
 }
