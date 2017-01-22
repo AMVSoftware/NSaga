8523e0575ba6d396e5ad56cf1109f47f2098af19
trailmax <trailmax1@gmail.com>
2017-01-22 00:03:51 +0000

    Sample for azure tables storage with SimpleInjector

--- src/Samples/AzureTableStorageSample.cs
+++ src/Samples/AzureTableStorageSample.cs
@@ -1,24 +1,47 @@
 ﻿using System;
 using NSaga;
 using NSaga.AzureTables;
+using NSaga.SimpleInjector;
+using SimpleInjector;
 
 namespace Samples
 {
     public class AzureTableStorageSample
     {
         private ISagaMediator sagaMediator;
-        private ISagaRepository sagaRepository;
 
         public void Run()
         {
             // You need Azure Storage emulator running to run this
+            var connectionString = "UseDevelopmentStorage=true";
             var builder = Wireup.UseInternalContainer()
                                 .UseRepository<AzureTablesSagaRepository>()
-                                .Register(typeof(ITableClientFactory), new TableClientFactory("UseDevelopmentStorage=true"));
+                                .Register(typeof(ITableClientFactory), new TableClientFactory(connectionString));
 
             sagaMediator = builder.ResolveMediator();
 
-            sagaRepository = builder.ResolveRepository();
+            var correlationId = Guid.NewGuid();
+            var initMessage = new PersonalDetailsVerification(correlationId)
+            {
+                FirstName = "James",
+                LastName = "Bond",
+            };
+
+            sagaMediator.Consume(initMessage);
+        }
+
+
+        public void WithSimpleInjector()
+        {
+            var container = new Container();
+            container.RegisterNSagaComponents();
+            container.UseSagaRepository<AzureTablesSagaRepository>();
+
+            // You need Azure Storage emulator running to run this
+            var connectionString = "UseDevelopmentStorage=true";
+            container.Register<ITableClientFactory>(()=> new TableClientFactory(connectionString), Lifestyle.Singleton);
+
+            sagaMediator = container.GetInstance<ISagaMediator>();
 
             var correlationId = Guid.NewGuid();
             var initMessage = new PersonalDetailsVerification(correlationId)
